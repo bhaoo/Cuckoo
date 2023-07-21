077397163d7b06ac8831137edb869b276fcd02e6
Bhao <45502567+bhaoo@users.noreply.github.com>
2023-07-21 14:25:19 +0800

    ⚡️ 支持评论头像懒加载

--- libs/comments.php
+++ libs/comments.php
@@ -118,7 +118,7 @@ class Cuckoo_Comments_Archive extends Widget_Abstract_Comments
                                               echo $commentClass;
                                               ?>">
       <div class="comment-image">
-        <img class="mdui-img-circle" src="<?php get_comment_avatar($this->mail); ?>" />
+        <img class="mdui-img-circle" src="<?php get_comment_avatar($this->mail); ?>" loading="lazy" />
         <?php get_comment_prefix($this->mail);
         if ($this->authorId == $this->ownerId) { ?>
           <img src="<?php staticFiles('images/grade/author.png', 0, 1) ?>" class="comment-prefix" mdui-tooltip="{content: '博主'}" />
